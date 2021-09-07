6301eb72fd0c9d779f7320de69e2944ef6b8ea2a
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-07 14:09:51 +0200

    ♻️ Switch to SCSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,9 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/css');
-  eleventyConfig.addWatchTarget('src/css');
+  eleventyConfig.addWatchTarget('src/scss');
   return {
     dir: {
       input: 'src',
     },
+    passthroughFileCopy: true,
   };
 };
