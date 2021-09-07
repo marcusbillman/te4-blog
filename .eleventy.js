cf5eba13f12664a23eb3dc2dd3948a7866142cb3
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-07 09:50:38 +0200

    🔧 Add .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/css');
+  eleventyConfig.addWatchTarget('src/css');
+  return {
+    dir: {
+      input: "src",
+    },
+  };
+};
