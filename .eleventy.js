2160f170356ad21ecddd07b7e047b32149998b75
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-09 09:40:46 +0200

    ➕ Add eleventy-plugin-syntaxhighlight

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('src/scss');
+  eleventyConfig.addPlugin(syntaxHighlight);
   return {
     dir: {
       input: 'src',
