c6770bc2b075889426f70163652d7cd4ddc46351
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-21 10:21:57 +0200

    ➕ Add eleventy-plugin-rss

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
+const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('src/scss');
+  eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addFilter('formattedDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
