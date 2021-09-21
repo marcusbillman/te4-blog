42fe89406a5e8049838e4845ab7df1befc64de4e
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-21 13:58:50 +0200

    ➕ Add eleventy-img

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,27 @@
+const Image = require('@11ty/eleventy-img');
 const pluginRss = require('@11ty/eleventy-plugin-rss');
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
 const { DateTime } = require('luxon');
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [400, 600],
+    formats: ['webp', 'jpeg'],
+    outputDir: './_site/img/',
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes: sizes || '100%',
+    loading: 'lazy',
+    decoding: 'async',
+  };
+
+  // Throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes, {
+    whitespaceMode: 'inline',
+  });
+}
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('src/scss');
@@ -9,6 +30,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter('formattedDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
+
   return {
     dir: {
       input: 'src',
