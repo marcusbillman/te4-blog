6eb47df2b3930df1b42a769dace48b934df94b61
Marcus Billman <54668482+marcusbillman@users.noreply.github.com>
2021-09-22 09:41:28 +0200

    ✨ repos.js: Cache GitHub API request

--- src/_data/repos.js
+++ src/_data/repos.js
@@ -1,13 +1,15 @@
-const axios = require('axios');
+const Cache = require('@11ty/eleventy-cache-assets');
 
 module.exports = async () => {
-  axios
-    .get('https://api.github.com/users/marcusbillman/repos')
-    .then((response) => {
-      const { data } = response;
-      return data;
-    })
-    .catch((error) => {
-      console.log(error);
+  const url = 'https://api.github.com/users/marcusbillman/repos';
+  try {
+    const data = Cache(url, {
+      duration: '1d',
+      type: 'json',
     });
+    return data;
+  } catch (e) {
+    console.log(e);
+    return {};
+  }
 };
